5ea14fcc3b104a5321628834d8ab3336b530faa9
Maykon <91572458+maykonwr@users.noreply.github.com>
2023-10-01 15:57:52 -0300

    code evolution

--- src/components/RequestInput/RequestInput.tsx
+++ src/components/RequestInput/RequestInput.tsx
@@ -1,20 +1,23 @@
+import { useState } from 'react'
 import { useUserContext } from '../../contexts/userContext'
 import { useParameters, handleRequestButton } from '../../hooks'
 import { Icon } from '../Icon'
 import './styles.css'
 
 function RequestInput() {
+    const [inputText, setInputText] = useState<string>('')
     const { setUser } = useUserContext()
     const { isActive, hasIcon, isFontSansSerif, text } = useParameters({
-        text: 'Banana'
+        text: inputText
     })
 
 
     return (
         <div className='RequestGroup'>
+            <input type='text' placeholder='Username' onChange={(element) => setInputText(element?.target?.value)}/>
             <button onClick={() => handleRequestButton(text, setUser)} disabled={!isActive} style={{fontFamily: isFontSansSerif ? 'sans-serif' : 'serif'}}>
                 {hasIcon && <Icon />}
-                {text}
+                Buscar
             </button>
         </div>
     )
